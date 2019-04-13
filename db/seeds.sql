d3896ce8113d7763cd0e76566f62b03e972f64da
Michael Diaz <poncediazdeleon@gmail.com>
2019-04-12 23:36:57 -0600

    upload

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name) VALUES ("Asada");
+INSERT INTO burgers (burger_name) VALUES ("Queso");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Marano", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Vegetariano", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Barbacoa", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Desebrada", true);
+INSERT INTO burgers (burger_name) VALUES ("Al Pastor");
+INSERT INTO burgers (burger_name) VALUES ("Poquito");
